a40b3bd911a78fce8273148dc5641396a8c03a0a
Piotr Lac <piotr.lac@protonmail.ch>
2018-01-15 23:39:22 +0100

    index if statement

--- app/controllers/adr_indicators_controller.rb
+++ app/controllers/adr_indicators_controller.rb
@@ -1,12 +1,15 @@
 class AdrIndicatorsController < ApplicationController
   def index
-    @adr_spis = AdrIndicator.all.select {|spi| spi.stakeholder == "EPKK"}
+    stake = "EPKK"
+    return @adr_spis = AdrIndicator.all.select {|spi| spi.stakeholder == stake} if stake != ""
+    @adr_spis = AdrIndicator.all
   end
 
   def show
   end
 
   def new
+    @adr_spi = AdrIndicator.new
   end
 
   def create
