b7336913936b87454fea63a3d947a76ab4e01e07
Piotr Lac <piotr.lac@protonmail.ch>
2018-01-16 20:19:21 +0100

    fix ops validation

--- app/models/ops_indicator.rb
+++ app/models/ops_indicator.rb
@@ -1,5 +1,5 @@
 class OpsIndicator < ApplicationRecord
-  validates :stakeholder, presence: true, length: { minimum: 4, maximum: 4 }
+  validates :stakeholder, presence: true
   validates :year, presence: true, length: { is: 4 }, numericality: { only_integer: true }
   validates :month, presence: true
   validates :number_of_operations, presence: true, numericality: { only_integer: true }  
