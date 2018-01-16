8bbd1f6cda4544d457723d68bfadfa23f9c46001
Piotr Lac <piotr.lac@protonmail.ch>
2018-01-16 12:47:41 +0100

    create migration ops_indicators

---
+++ db/migrate/20180116114616_create_ops_indicators.rb
@@ -0,0 +1,22 @@
+class CreateOpsIndicators < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ops_indicators do |t|
+      t.string :stakeholder
+      t.integer :year
+      t.string :month
+      t.integer :number_of_operations
+      t.integer :rwy_excursion
+      t.integer :twy_excursion
+      t.integer :app_excursion
+      t.integer :arc
+      t.integer :fumes_smoke
+      t.integer :fire
+      t.integer :taws
+      t.integer :stall_warning
+      t.integer :level_bust
+      t.integer :tcas_ra
+      t.integer :scf_np
+      t.integer :scf_pp
+    end
+  end
+end
