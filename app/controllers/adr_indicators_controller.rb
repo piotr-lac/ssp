c05d5bb36ee0961ee63ff6ff9ce6436a06f742c7
Piotr Lac <piotr.lac@protonmail.ch>
2018-01-16 22:58:15 +0100

    add permit to params

--- app/controllers/adr_indicators_controller.rb
+++ app/controllers/adr_indicators_controller.rb
@@ -13,8 +13,9 @@ class AdrIndicatorsController < ApplicationController
   end
 
   def create
-    @adr_spi = AdrIndicator.new(adr_spi_params)
+    @adr_spi = AdrIndicator.new(params.require(:adr_indicator).permit!)
     @adr_spi.save
+    redirect_to adr_indicators_path
   end
 
   def edit
@@ -28,6 +29,19 @@ class AdrIndicatorsController < ApplicationController
 
   private
 
-  def adr_spi_params
-    params.require(::).permit()
+  def permit!
+    each_pair do |key, value|
+      convert_hashes_to_parameters(key, value)
+      self[key].permit! if self[key].respond_to? :permit!
+    end
+
+    # @permitted = true
+    self
+  end
+
+
+  # def adr_spi_params
+  #   @atr = AdrIndicator.attribute_names.map {|x| x.to_sym}
+  #   params.require(:adr_indicator).permit(:year, :month, :stakeholder, :number_of_operations, :rwy_incursion, :twy_incursion, :app_incursion, :rwy_excursion, :twy_excursion, :app_excursion, :fuel, :fsf, :gcol, :ramp, :obstacle, :birdstrike, :wildlife, :wild_take_abort, :go_around, :land_rvr, :take_of_rvr, :gcol_ramp_lvp, :rwy_incursion_spi)
+  # end
 end
