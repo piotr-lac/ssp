3216c803f5d6eeb699eb5fdcefa2f40a7d85d714
Piotr Lac <piotr.lac@protonmail.ch>
2018-01-16 20:23:33 +0100

    add ahac,atm,ato validation

--- app/models/ahac_indicator.rb
+++ app/models/ahac_indicator.rb
@@ -1,2 +1,6 @@
 class AhacIndicator < ApplicationRecord
+  validates :stakeholder, presence: true
+  validates :year, presence: true, length: { is: 4 }, numericality: { only_integer: true }
+  validates :month, presence: true
+  validates :number_of_operations, presence: true, numericality: { only_integer: true } 
 end
--- app/models/atm_indicator.rb
+++ app/models/atm_indicator.rb
@@ -1,2 +1,6 @@
 class AtmIndicator < ApplicationRecord
+  validates :stakeholder, presence: true
+  validates :year, presence: true, length: { is: 4 }, numericality: { only_integer: true }
+  validates :month, presence: true
+  
 end
