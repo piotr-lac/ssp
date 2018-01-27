dba0e9a3a196c1d473b5a038fcb28b54ce06b1a9
Piotr Lac <piotr.lac@protonmail.ch>
2018-01-27 15:32:33 +0100

    add atm atp controller adr index, ahac show, atm index, ato index

--- app/controllers/atm_indicators_controller.rb
+++ app/controllers/atm_indicators_controller.rb
@@ -1,23 +1,46 @@
 class AtmIndicatorsController < ApplicationController
   def index
+    @atm_spis = AtmIndicator.all
   end
 
   def show
+     @atm_indicator = AtmIndicator.find(params[:id])
   end
 
   def new
+     @atm_indicator = AtmIndicator.new
   end
 
   def create
+    @atm_indicator = AtmIndicator.new(params.require(:atm_indicator).permit!)
+    @atm_indicator.save
+    redirect_to atm_indicators_path
   end
 
   def edit
+    @atm_indicator = AtmIndicator.find(params[:id])
   end
 
   def update
+    @atm_indicator = AtmIndicator.find(params[:id])
+    @atm_indicator.update(params.require(:atm_indicator).permit!)
+    redirect_to atm_indicators_path
   end
 
   def destroy
+    @atm_indicator = AtmIndicator.find(params[:id])
+    @atm_indicator.destroy
+    redirect_to atm_indicators_path
   end
   
+  private
+
+  def permit!
+    each_pair do |key, value|
+      convert_hashes_to_parameters(key, value)
+      self[key].permit! if self[key].respond_to? :permit!
+    end
+    self
+  end
+
 end
--- app/controllers/ato_indicators_controller.rb
+++ app/controllers/ato_indicators_controller.rb
@@ -1,23 +1,46 @@
 class AtoIndicatorsController < ApplicationController
   def index
+     @ato_spis = AtoIndicator.all
   end
 
   def show
+     @ato_indicator = AtoIndicator.find(params[:id])
   end
 
   def new
+    @ato_indicator = AtoIndicator.new
   end
 
   def create
+    @ato_indicator = AtoIndicator.new(params.require(:ato_indicator).permit!)
+    @ato_indicator.save
+    redirect_to ato_indicators_path
   end
 
   def edit
+     @ato_indicator = AtoIndicator.find(params[:id])
   end
 
   def update
+    @ato_indicator = AtoIndicator.find(params[:id])
+    @ato_indicator.update(params.require(:ato_indicator).permit!)
+    redirect_to ato_indicators_path
   end
 
   def destroy
+    @ato_indicator = AtoIndicator.find(params[:id])
+    @ato_indicator.destroy
+    redirect_to ato_indicators_path
+  end
+
+  private
+
+  def permit!
+    each_pair do |key, value|
+      convert_hashes_to_parameters(key, value)
+      self[key].permit! if self[key].respond_to? :permit!
+    end
+    self
   end
   
 end
